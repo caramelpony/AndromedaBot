5fd5d4ab032fdc473a9bcb324257bf8df5dcca3f
Aurorah <aurorahHarmony@gmail.com>
2023-01-06 22:09:01 +1300

    Cleanup the SL bot session on app close

--- src/app.ts
+++ src/app.ts
@@ -16,3 +16,25 @@ slBot.run();
 const dBot = new DiscordBot(serviceContainer);
 serviceContainer.setDiscordBot(dBot);
 dBot.run();
+
+// Cleanup on Bot shutdown. There may be rare cases where this does not catch the exit event, for example on an exception.
+async function exitHandler(options: any, exitCode: any) {
+  if (options.cleanup) {
+    await serviceContainer.getSlBot()?.getBot().close();
+  };
+  if (exitCode || exitCode === 0) console.log(exitCode);
+  if (options.exit) process.exit();
+}
+
+//do something when app is closing
+process.on('exit', exitHandler.bind(null, { cleanup: true }));
+
+//catches ctrl+c event
+process.on('SIGINT', exitHandler.bind(null, { cleanup: true, exit: true }));
+
+// catches "kill pid" (for example: nodemon restart)
+process.on('SIGUSR1', exitHandler.bind(null, { cleanup: true, exit: true }));
+process.on('SIGUSR2', exitHandler.bind(null, { cleanup: true, exit: true }));
+
+//catches uncaught exceptions
+process.on('uncaughtException', exitHandler.bind(null, { cleanup: true, exit: true }));
